4bc80a1ba38b705766fa55bc8fa30f4f475150c6
Sidoryx <siddhantban02@gmail.com>
2022-06-27 09:45:34 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let currentSlide=0;
+const slides=document.getElementsByClassName('carouselItem');
+let totalSlides=slides.length;
+console.log(slides.length);
+
+document.getElementById('prevBtn').onclick=function(){
+    if(currentSlide == 0){
+        currentSlide = slides.length-1;
+    }
+    else
+    {
+        currentSlide --;
+    }
+    updateSlide();
+}
+
+document.getElementById('nextBtn').onclick=function(){
+    if(currentSlide === slides.length-1){
+        currentSlide = 0;
+    }
+    else
+    {
+        currentSlide ++;
+    }
+    updateSlide();
+}
+
+function updateSlide(){
+    // slides.classList.remove("carouselItemVisible");
+    for(let slide of slides){
+        slide.classList.remove('carouselItemVisible');
+        slide.classList.add('carouselItemHidden');
+    }
+    slides[currentSlide].classList.add("carouselItemVisible");
+}
